3b4c16884cbb73a8f6fb8574a87e1d5c3344a445
MEZ901 <issammez44@gmail.com>
2023-09-08 16:29:33 +0100

    create the find method in the Model class

--- src/main/java/LibroSyncApp.java
+++ src/main/java/LibroSyncApp.java
@@ -2,6 +2,9 @@ package src.main.java;
 
 import src.main.java.repository.Model;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class LibroSyncApp {
@@ -9,14 +12,19 @@ public class LibroSyncApp {
         Scanner s = new Scanner(System.in);
         int choice;
 
-        System.out.println("\n\n\t\t Welcome to LibroSync \t\t\n");
         do {
-            System.out.println("\n1. Display all books");
-            System.out.println("2. Add new book");
-            System.out.println("3. Update book");
-            System.out.println("4. Delete book");
-            System.out.println("5. Search for books by title or author");
-            System.out.println("0. Exit\n");
+            System.out.println("+--------------------------------------------+");
+            System.out.println("|          Library Management System         |");
+            System.out.println("+--------------------------------------------+");
+            System.out.println("|  Choice  |           Description           |");
+            System.out.println("+--------------------------------------------+");
+            System.out.println("|     1    | Display all books               |");
+            System.out.println("|     2    | Add new book                    |");
+            System.out.println("|     3    | Update book                     |");
+            System.out.println("|     4    | Delete book                     |");
+            System.out.println("|     5    | Search for books by title/author|");
+            System.out.println("|     0    | Exit                            |");
+            System.out.println("+--------------------------------------------+");
             System.out.print("Enter your choice: ");
 
             choice = s.nextInt();
@@ -25,6 +33,15 @@ public class LibroSyncApp {
                 case 1:
                     break;
                 case 2:
+                    Model model = new Model();
+                    Map<String, Object> criteria = new HashMap<>();
+                    criteria.put("isbn", "9783104012544");
+
+                    String tableName = "book";
+
+                    List<Map<String, Object>> results = model.find(tableName, criteria);
+
+                    System.out.println(results);
                     break;
             }
         } while (choice != 0);
--- src/main/java/repository/Model.java
+++ src/main/java/repository/Model.java
@@ -39,7 +39,54 @@ public class Model {
         return resultList;
     }
 
-//    public List<Map<String, Object>> find(String table ,Map<String, Object> data) {}
-//
+    public List<Map<String, Object>> find(String table, Map<String, Object> data) {
+        List<Map<String, Object>> resultList = new ArrayList<>();
+
+        DatabaseConnection dbConnection = new DatabaseConnection();
+        Connection connection = dbConnection.getConnection();
+
+        if (connection != null) {
+            try (Connection conn = connection) {
+                StringBuilder queryBuilder = new StringBuilder("SELECT * FROM ");
+                queryBuilder.append(table).append(" WHERE ");
+
+                for (String key : data.keySet()) {
+                    queryBuilder.append(key).append(" = ? AND ");
+                }
+                queryBuilder.delete(queryBuilder.length() - 5, queryBuilder.length());
+
+                try (PreparedStatement stmt = conn.prepareStatement(queryBuilder.toString())) {
+                    int parameterIndex = 1;
+                    for (String key : data.keySet()) {
+                        stmt.setObject(parameterIndex++, data.get(key));
+                    }
+
+                    try (ResultSet rs = stmt.executeQuery()) {
+                        ResultSetMetaData metaData = rs.getMetaData();
+                        int columnCount = metaData.getColumnCount();
+
+                        while (rs.next()) {
+                            Map<String, Object> row = new HashMap<>();
+                            for (int i = 1; i <= columnCount; i++) {
+                                String columnName = metaData.getColumnName(i);
+                                Object columnValue = rs.getObject(i);
+                                row.put(columnName, columnValue);
+                            }
+                            resultList.add(row);
+                        }
+                    }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                dbConnection.closeConnection();
+            }
+        } else {
+            System.out.println("Failed to establish a database connection.");
+        }
+
+        return resultList;
+    }
+
 //    public String insert(Map<String, Object> data){}
 }
