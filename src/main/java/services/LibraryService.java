6622cd08327ee514bd9a9b12f35a6faf87ebc48b
MEZ901 <issammez44@gmail.com>
2023-09-09 14:17:04 +0100

    add utility method to clear the console

--- src/main/java/services/LibraryService.java
+++ src/main/java/services/LibraryService.java
@@ -1,17 +1,23 @@
 package src.main.java.services;
 
 import src.main.java.repository.Model;
+import src.main.java.utilities.ConsoleClear;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class LibraryService {
     Model model = new Model("book");
 
     public void displayAllBooks() {
         List<Map<String, Object>> books = model.getAll();
+        Scanner s = new Scanner(System.in);
+        int choice;
 
-        System.out.println("+------------------------------------------------------------------------------+");
+//        ConsoleClear.clearConsole();
+
+        System.out.println("\n+------------------------------------------------------------------------------+");
         System.out.println("|  Quantity |       ISBN      |         Title        | Author ID |    Status   |");
         System.out.println("+------------------------------------------------------------------------------+");
 
@@ -21,6 +27,25 @@ public class LibraryService {
                     row.get("author_id"), row.get("status"));
         }
 
-        System.out.println("+------------------------------------------------------------------------------+");
+        System.out.println("+------------------------------------------------------------------------------+\n");
+
+        System.out.println("1. Back to menu");
+        System.out.println("0. Exit");
+        do {
+            System.out.print("Enter your choice: ");
+
+            choice = s.nextInt();
+
+            switch (choice) {
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1:
+                    break;
+                default:
+                    System.out.println("You can only choose between 1 or 0");
+                    break;
+            }
+        } while(choice != 1);
     }
 }
---
+++ src/main/java/utilities/ConsoleClear.java
@@ -0,0 +1,19 @@
+package src.main.java.utilities;
+
+import java.io.IOException;
+
+public class ConsoleClear {
+    public static void clearConsole() {
+        try {
+            final String os = System.getProperty("os.name");
+
+            if (os.contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
